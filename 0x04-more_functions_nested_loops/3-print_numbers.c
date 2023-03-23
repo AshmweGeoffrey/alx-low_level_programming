75adf36eb5246611ef96fc78a08658884b038681
geoffrey <ashimwegeoffrey@gmail.com>
2023-03-23 09:29:43 -0700

    /alx-low_level_programming/0x04-more_functions_nested_loops#

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+int print_numbers(void)
+{
+        int i;
+
+        i = 0;
+        while (i < 10)
+        {
+        putchar(i + '0');
+        i++;
+        }
+        putchar('\n');
+        return (0);
+}
